31beefd6609c6f5c7c6f39c7de459b21562a29bf
sneh2692007 <132449345+sneh2692007@users.noreply.github.com>
2023-05-10 15:17:15 +0530

    Add files via upload

---
+++ M1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+     clrscr();
+     int p,g,m;
+     printf("enter your number");
+     scanf("%d",&p);
+     printf("plz enter even number");
+     printf("2,4,6,8");
+     scanf("%d",&g);
+     printf("plz enter odd number");
+     printf("1,3,5,7");
+     scanf("%d",&m);
+     getch();
+}
